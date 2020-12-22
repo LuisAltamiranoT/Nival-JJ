687170f61d63446193dc9126f63ea7bb96c0ea2a
unknown <Administrator@casapazmino.com>
2020-12-22 01:58:18 -0500

    'prueba-heroku-2'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDSmVOfRQPPs0uCz9jCmxOqZzN7DYMzw14",
+    authDomain: "easyacnival.firebaseapp.com",
+    databaseURL: "https://easyacnival.firebaseio.com",
+    projectId: "easyacnival",
+    storageBucket: "easyacnival.appspot.com",
+    messagingSenderId: "719741592491",
+    appId: "1:719741592491:web:2f0121f4fe24f53d4aa8ee",
+    measurementId: "G-981GNVNGDT"
+  }
 };
